e7071cb649c924ceb1a9da083005536ae241c7e0
Hamid Azim <hamidazim321@gmail.com>
2023-08-29 15:26:11 +0500

    Fix bugs

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -11,27 +11,37 @@ const Missions = () => {
 
   let content;
 
-  if (!pending && !error) {
+  if (!pending && !error && Array.isArray(missions)) {
     content = (
       <table className="missions-table">
-        <tr key="missions">
-          <th>Mission</th>
-          <th>Description</th>
-          <th>Status</th>
-          <th>
-            {' '}
-
-          </th>
-        </tr>
-        {missions.map((mission) => (
-          <tr key={mission.mission_id}>
-            <td>{mission.mission_name}</td>
-            <td>{mission.description}</td>
-            <td><button type="button">Not a member</button></td>
-            <td><button type="button">Join as member</button></td>
+        <tbody>
+          <tr key="missions">
+            <th>Mission</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th>{' '}</th>
           </tr>
-        ))}
+          {missions.map((mission) => (
+            <tr key={mission.mission_id}>
+              <td>{mission.mission_name}</td>
+              <td>{mission.description}</td>
+              <td><button type="button">Not a member</button></td>
+              <td><button type="button">Join as member</button></td>
+            </tr>
+          ))}
+        </tbody>
       </table>
+    );
+  }
+
+  if (pending) {
+    content = (
+      <h1>Fetching Missions</h1>
+    );
+  }
+  if (error) {
+    content = (
+      <h1>Error occured while fetching missions</h1>
     );
   }
   return (
