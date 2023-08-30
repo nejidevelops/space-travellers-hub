edfc2d6280b6512124377d1696118651c9408e67
Newton Ombese <newtonombese1@gmail.com>
2023-08-30 10:08:00 +0300

    Add my rocket redux to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import missionsReducer from './Missions/missions';
+import rocketsReducer from './Rockets/rockets';
 
 const store = configureStore({
   reducer: {
     missions: missionsReducer,
+    rockets: rocketsReducer,
   },
 });
 
