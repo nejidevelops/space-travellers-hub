13a5ead75922d76644e6d89792799416d014e185
Hamid Azim <hamidazim321@gmail.com>
2023-08-30 13:35:05 +0500

    Display reserved missions in the Profile Section

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,18 @@
-const MyProfile = () => (
-  <h1>My Profile</h1>
-);
+import { useSelector } from 'react-redux';
+
+const MyProfile = () => {
+  const { missions } = useSelector(((state) => state.missions));
+  const reserved = missions.filter((mission) => mission.reserved === true);
+  return (
+    <div className="my-missions">
+      <h1>My Missions</h1>
+      <ul>
+        {reserved.map((mission) => (
+          <li key={mission.id}>{mission.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default MyProfile;
