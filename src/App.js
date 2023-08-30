fc41a681fc754828edd9cc4877d18307bc3ad06f
Hamid Azim <hamidazim321@gmail.com>
2023-08-30 13:13:56 +0500

    Add MyProfile Component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Missions from './components/Missions';
 import Navbar from './components/Navbar';
+import MyProfile from './components/Profile';
 
 function App() {
   return (
     <main className="App">
       <Navbar />
       <Routes>
+        <Route element={<MyProfile />} path="/" />
         <Route element={<Missions />} path="/missions" />
       </Routes>
     </main>
