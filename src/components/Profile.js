68ab61494caaef2a6a21b4df3cd374e06731efb8
Hamid Azim <hamidazim321@gmail.com>
2023-08-30 14:21:03 +0500

    Add styles to reserved missions section

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,16 +1,20 @@
 import { useSelector } from 'react-redux';
+import '../styles/Profile.css';
 
 const MyProfile = () => {
   const { missions } = useSelector(((state) => state.missions));
   const reserved = missions.filter((mission) => mission.reserved === true);
   return (
-    <div className="my-missions">
-      <h1>My Missions</h1>
-      <ul>
-        {reserved.map((mission) => (
-          <li key={mission.id}>{mission.name}</li>
-        ))}
-      </ul>
+    <div className="my-profile">
+      <div className="my-missions">
+        <p className="missions-headline">My Missions</p>
+        <ul className="missions-list">
+          {reserved.map((mission) => (
+            <li key={mission.id}>{mission.name}</li>
+          ))}
+        </ul>
+      </div>
+      <div className="my-rockets" />
     </div>
   );
 };
