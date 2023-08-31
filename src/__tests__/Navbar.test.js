7ef2aff000a6d12d63d8f986a0b7d2c12f3dc8a4
Hamid Azim <hamidazim321@gmail.com>
2023-08-31 17:01:24 +0500

    Add snapshot test for Navbar component

---
+++ src/__tests__/Navbar.test.js
@@ -0,0 +1,34 @@
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import configureStore from 'redux-mock-store';
+import Navbar from '../components/Navbar';
+
+const mockStore = configureStore([]);
+
+describe('Navbar', () => {
+  test('Navbar rendered correctly', () => {
+    const store = mockStore({
+      missions: {
+        missions:[
+          {
+            name: 'Test Mission',
+            description: 'Test Description',
+            reserved: false,
+            id: 'test-id',
+          }
+        ],
+        pending: false,
+        error: false,
+      },
+    });
+    const navbar = render(
+      <Provider store={store}>
+        <Router>
+          <Navbar />
+        </Router>
+      </Provider>,
+    );
+    expect(navbar).toMatchSnapshot();
+  });
+});
