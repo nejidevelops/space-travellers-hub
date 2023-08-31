cfc7c4b811fb9946a298d5cf33be82cbcd28ee9d
Hamid Azim <hamidazim321@gmail.com>
2023-08-31 18:44:23 +0500

    Add logger middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 import missionsReducer from './Missions/missions';
 import rocketsReducer from './Rockets/rockets';
 
@@ -7,6 +8,7 @@ const store = configureStore({
     missions: missionsReducer,
     rockets: rocketsReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
