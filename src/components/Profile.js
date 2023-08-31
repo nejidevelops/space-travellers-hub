921e87c8b1c0459ba10c72b0e24365ead803c06f
Hamid Azim <hamidazim321@gmail.com>
2023-08-31 18:22:35 +0500

    Display message if profile section is empty

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,23 +6,46 @@ const MyProfile = () => {
   const reserved = missions.filter((mission) => mission.reserved === true);
   const { rockets } = useSelector((state) => state.rockets);
   const reservedRockets = rockets.filter((rocket) => rocket.reserved === true);
+
+  let missionList;
+  let rocketList;
+
+  if (reserved.length > 0) {
+    missionList = (
+      <ul className="missions-list">
+        {reserved.map((mission) => (
+          <li key={mission.id}>{mission.name}</li>
+        ))}
+      </ul>
+    );
+  } else {
+    missionList = (
+      <p className="empty-profile-msg">You have not joined any missions yet</p>
+    );
+  }
+
+  if (reservedRockets.length > 0) {
+    rocketList = (
+      <ul className="rockets-list">
+        {reservedRockets.map((rocket) => (
+          <li key={rocket.id}>{rocket.name}</li>
+        ))}
+      </ul>
+    );
+  } else {
+    rocketList = (
+      <p className="empty-profile-msg">You have not reserved any Rockets yet</p>
+    );
+  }
   return (
     <div className="my-profile">
       <div className="my-missions">
         <p className="missions-headline">My Missions</p>
-        <ul className="missions-list">
-          {reserved.map((mission) => (
-            <li key={mission.id}>{mission.name}</li>
-          ))}
-        </ul>
+        {missionList}
       </div>
       <div className="my-rockets">
         <p className="rockets-headline">My Rockets</p>
-        <ul className="rockets-list">
-          {reservedRockets.map((rocket) => (
-            <li key={rocket.id}>{rocket.name}</li>
-          ))}
-        </ul>
+        {rocketList}
       </div>
     </div>
   );
