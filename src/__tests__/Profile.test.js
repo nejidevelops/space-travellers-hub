9ab13c969315e426a083988750abc25ede2d914c
Hamid Azim <hamidazim321@gmail.com>
2023-08-31 17:20:26 +0500

    Add test to only display reserved rockets and functions in the Profile section

--- src/__tests__/Profile.test.js
+++ src/__tests__/Profile.test.js
@@ -1,3 +1,5 @@
+import '@testing-library/jest-dom/extend-expect';
+import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
@@ -5,7 +7,7 @@ import MyProfile from '../components/Profile';
 
 const mockStore = configureStore([]);
 
-describe('MissionCard', () => {
+describe('Profile', () => {
   let store;
   beforeEach(()=> {
     store = mockStore({
@@ -55,5 +57,16 @@ describe('MissionCard', () => {
       </Provider>,
     );
     expect(myProfile).toMatchSnapshot();
+  });
+
+  test('Only display reserved rockets and missions', ()=> {
+    const { getByText } = render(
+      <Provider store={store}>
+        <MyProfile />
+      </Provider>,
+    );
+  
+    expect(getByText('Test Mission 2')).toBeInTheDocument();
+    expect(getByText('Test Rocket 1')).toBeInTheDocument();
   });
 });
